67c963986de5319ecfc5e36459dda7ef615536d6
YennhiDUT <91811893+YennhiDUT@users.noreply.github.com>
2023-08-11 13:39:47 +0700

    create component in src

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export * as actionType from "./actions"; //for all actions
+export * as constants from "./constants"; // for all misc constants
---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+import authReducer from "./authReducer";
+import basketReducer from "./basketReducer";
+import filterReducer from "./filterReducer";
+import productReducer from "./productReducer";
+import categoryReducer from "./categoryReducer";
+import searchReducer from "./searchReducer";
+
+//creating a single rootEeducer object
+const rootReducer = {
+  auth: authReducer,
+  basket: basketReducer,
+  filter: filterReducer,
+  product: productReducer,
+  category: categoryReducer,
+  search: searchReducer,
+};
+
+export default rootReducer;
